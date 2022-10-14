5066e77a6a28bd58be3ba88ebe9c802e9fb14b77
SibghatullahDurrani <49807756+SibghatullahDurrani@users.noreply.github.com>
2022-10-14 03:45:20 -0700

    Update _dbConnectUsers.php

--- partials/_dbConnectUsers.php
+++ partials/_dbConnectUsers.php
@@ -1,8 +1,11 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "data1";
+$server = "eu-cdbr-west-03.cleardb.net";
+$username = "b48bf0586d111b";
+$password = "9a4561fc";
+$database = "heroku_f848ce884e918c9";
+
+$active_group = 'default';
+$query_builder = TRUE;
 
 $connect = mysqli_connect($server, $username, $password, $database);
 
